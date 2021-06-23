95a1b91d9ce39fb29df48da7388648bc10e984ea
ChengHsuanLiu <chenghsuango@gmail.com>
2021-06-23 20:09:15 +0800

    update

--- assets/js/map.js
+++ assets/js/map.js
@@ -1,4 +1,6 @@
-var myCenter=new google.maps.LatLng(25.0332786,121.5494237);
+var myCenter=new google.maps.LatLng(25.0329562,121.5549477);
+
+// 25.0332738,121.549429
 
     function initialize()
     {
@@ -6,78 +8,25 @@ var myCenter=new google.maps.LatLng(25.0332786,121.5494237);
             center:myCenter,
             scrollwheel: false,
             zoom:17,
-            // mapTypeId:google.maps.MapTypeId.ROADMAP
+            mapTypeId:google.maps.MapTypeId.ROADMAP
         };
         var map=new google.maps.Map(document.getElementById("googleMap"),mapProp);
         var marker=new google.maps.Marker({
             position:myCenter,
-            map: map,
+            map: map
         });
 
-		var styles =  [
-			{
-				"stylers": [
-					{
-						"hue": "#C70909"
-					},
-					{
-						"saturation": 10
-					}
-				]
-			},
-			{
-				"featureType": "water",
-				"stylers": [
-					{
-						"color": "#effefd"
-					}
-				]
-			},
-			{
-				"featureType": "all",
-				"elementType": "labels",
-				"stylers": [
-					{
-						"visibility": "off"
-					}
-				]
-			},
-			{
-				"featureType": "administrative",
-				"elementType": "labels",
-				"stylers": [
-					{
-						"visibility": "on"
-					}
-				]
-			},
-			{
-				"featureType": "road",
-				"elementType": "all",
-				"stylers": [
-					{
-						"visibility": "off"
-					}
-				]
-			},
-			{
-				"featureType": "transit",
-				"elementType": "all",
-				"stylers": [
-					{
-						"visibility": "off"
-					}
-				]
-			}
-		];
-
-
-
-
-
-
-        map.setOptions({styles: styles});
         marker.setMap(map);
+
+        var infowindow = new google.maps.InfoWindow({
+    				content: "<div><h5>台北市大安區信義路四段415號12樓</h5></div>",
+			  });
+
+			  infowindow.open({
+		      anchor: marker,
+		      map,
+		      shouldFocus: false,
+		    });
     }
 google.maps.event.addDomListener(window, 'load', initialize);
 
